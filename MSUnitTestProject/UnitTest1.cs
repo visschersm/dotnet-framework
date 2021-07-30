4c9ea735349df637fe5a22337387c03b785aaeae
Mark Visschers <visschers.m@gmail.com>
2021-07-30 12:47:51 +0200

    failing unit test

--- MSUnitTestProject/UnitTest1.cs
+++ MSUnitTestProject/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace MSUnitTestProject
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.IsTrue(true);
+            Assert.IsTrue(false);
         }
     }
 }
